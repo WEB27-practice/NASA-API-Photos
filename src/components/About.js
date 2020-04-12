a74254010aea8ea0b1efc1a9ef70485c8881fc0d
seanaleid <seanaleid@gmail.com>
2020-04-11 22:46:45 -0400

    Add text to the About.js
      * Adds some information to the description

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,38 @@ import React from "react";
 
 import styled from "styled-components" ;
 
-const About = () => {
+const PhotoBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 2rem;
+    padding: 2rem;
+    background-color: rgba(0, 0, 0, .5);
+    border-radius: 0.5rem;
+    opacity: 0.5;
+    :hover {
+        opacity: 1;
+    }
+`;
 
+const Title = styled.h1`
+    color: #fff;
+`;
+
+const Text = styled.p`
+    color: #fff;
+`;
+
+const About = () => {
 
     return (
         <div>
-            <h1>THIS IS A TEST</h1>
+            <PhotoBox>
+                <Title>NASA API Project</Title>
+                <Text>Tech used: HTML, CSS, JS</Text>
+                <Text>I was first challenged do complete this project as a student.</Text>
+            </PhotoBox>
         </div>
     )
 }
