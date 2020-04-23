20efb084c626ff58e7e9bba11ceda5c88fa50df7
seanaleid <seanaleid@gmail.com>
2020-04-23 13:19:43 -0400

    Add initial structure for a modal

--- src/components/Container.js
+++ src/components/Container.js
@@ -3,6 +3,17 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import styled from "styled-components";
 
+const Modal = styled.div`
+    margin: auto;
+    background-color:#fff;
+    height: 30rem;
+    width: 30rem;
+    border-radius: 1rem;
+    padding: 1rem;
+    z-index: 10000;
+    visibility: hidden;
+`;
+
 const DateBox = styled.div`
 display: flex;
 flex-direction: column;
@@ -12,6 +23,7 @@ margin: 2rem;
 padding: 2rem;
 border-radius: 0.5rem;
 opacity: 0.5;
+z-index: -2;
 :hover {
     opacity: 1;
 }
@@ -22,6 +34,7 @@ const PhotoBox = styled.div`
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    z-index: -2;
     margin: 2rem;
     padding: 2rem;
     background-color: rgba(0, 0, 0, .5);
@@ -56,6 +69,7 @@ const Container = () => {
 
     const [daily, setDaily] = useState([]);
     const [newDate, setNewDate] = useState(`2014-05-11`);
+    const [openModal, setOpenModal] = useState(true);
 
     useEffect(() => {
         
@@ -100,8 +114,22 @@ const Container = () => {
         return (!copyright ? '' : test);
     }
 
+    function closeModal() {
+        setOpenModal(false);
+    }
+
     return (
         <div>
+            // the modal 
+            <Modal id="myModal" className="modal">
+
+                // Modal content
+                <div className="modal-content">
+                    <span className="close" onClick={closeModal}>&times;</span>
+                    <p>Some text in the Modal..</p>
+                </div>
+
+            </Modal>
             <DateBox>
                 <form className="nasa-form" onSubmit={handleSubmit}>
                 <div className="form-box">
